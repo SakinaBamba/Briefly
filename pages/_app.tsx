2f184bf7f85f248ad463ecd9ce4934ff0982429f
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-04-11 08:25:56 -0400

    Removed styles global import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
+
 export default function App({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
