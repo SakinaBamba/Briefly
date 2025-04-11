692a9e15e61ad5aa6c9401e8b6c875fb503e33fc
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-04-11 08:24:57 -0400

    Update _app.tsx
    Removed style global import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
-
 export default function App({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
