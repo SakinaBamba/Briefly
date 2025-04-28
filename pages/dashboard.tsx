63e5b23d2eaa5effc1d19fd35c2e2665e529916a
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-04-28 17:32:17 -0400

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
