042bcadd736cc761698ac913078b15786094a9a7
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-04-28 17:30:21 -0400

    Update callback.tsx

--- pages/auth/callback.tsx
+++ pages/auth/callback.tsx
@@ -2,6 +2,7 @@ import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
 import { GetServerSidePropsContext } from 'next';
 import { redirect } from 'next/navigation';
 
+
 export default function Callback() {
   redirect('/dashboard'); // fallback redirect (if needed)
   return null;
