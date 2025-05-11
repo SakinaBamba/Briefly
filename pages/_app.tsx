c1cb55337ccb53a755792fe69a0041dafda98654
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-05-10 23:53:49 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { createBrowserClient } from '@supabase/ssr';
 import type { AppProps } from 'next/app';
 
+
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() =>
     createBrowserClient(
