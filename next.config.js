30abe9a2d6075441525584ac4ae88dc945118b26
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-05-11 17:02:24 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 // next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Prevent Next.js from issuing 307 redirects
+  // for missing or extra trailing slashes in API routes
+  trailingSlash: false,
+
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 }
+
+module.exports = nextConfig
