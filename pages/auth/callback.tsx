83378ebde6bab803fcaa08d325caee4b69aaf93d
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-05-10 23:35:55 -0400

    Update callback.tsx

--- pages/auth/callback.tsx
+++ pages/auth/callback.tsx
@@ -1,12 +1,22 @@
 // pages/auth/callback.tsx
 import { GetServerSidePropsContext } from 'next';
 import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers'; // Not usable in GSSP, instead use below polyfill
+import type { CookieOptionsWithName, CookieMethodsServer } from '@supabase/ssr';
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: ctx.req.cookies }
+    {
+      cookies: {
+        get: (key: string) => ctx.req.cookies[key],
+        set: (key, value, options) =>
+          ctx.res.setHeader('Set-Cookie', `${key}=${value}; Path=${options.path ?? '/'}; HttpOnly`),
+        remove: (key, options) =>
+          ctx.res.setHeader('Set-Cookie', `${key}=; Path=${options.path ?? '/'}; Max-Age=0`),
+      } satisfies CookieMethodsServer,
+    }
   );
 
   const {
