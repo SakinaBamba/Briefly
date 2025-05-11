f193b715b1ce3fcad40bbf50904a9a04496f2165
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-05-11 01:40:23 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-const nextConfig = {
-  reactStrictMode: true,
-};
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+}
