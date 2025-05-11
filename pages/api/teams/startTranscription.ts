d8dd8fb2db080a91a4dd1b4cd046dfa264c8cc40
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-05-10 22:48:34 -0400

    Update startTranscription.ts

--- pages/api/teams/startTranscription.ts
+++ pages/api/teams/startTranscription.ts
@@ -3,6 +3,7 @@
 import { useEffect } from 'react';
 import * as microsoftTeams from '@microsoft/teams-js';
 
+
 export default function StartTranscription() {
   useEffect(() => {
     microsoftTeams.app.initialize().then(() => {
