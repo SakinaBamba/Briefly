c031d05b73c869efa01c1ef18e8ad909fb294f6d
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-05-26 03:08:52 -0400

    Update pollCallRecords.ts

--- pages/api/graph/pollCallRecords.ts
+++ pages/api/graph/pollCallRecords.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from '@supabase/supabase-js'
 import { getGraphAccessToken } from '../../../utils/getGraphToken'
 
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // Must use service role to insert
