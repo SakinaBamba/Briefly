6cb7b420a8ff35946e6945f3734c78feb6c6f0c9
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-06-04 23:07:27 -0400

    Update supabaseClient.ts

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export const supabase = createBrowserSupabaseClient({
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+});
