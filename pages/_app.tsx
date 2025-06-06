15f63c3c8bd92debfb1935f09e9b7a0e6ea740f3
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-06-06 00:20:22 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,6 @@
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [supabase] = useState(() =>
-    createBrowserSupabaseClient({
-      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    })
-  )
-
-  return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  )
+  return <Component {...pageProps} />
 }
