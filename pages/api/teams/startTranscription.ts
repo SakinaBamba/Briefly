08cd09742096590946a912635237628caffab196
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-06-06 01:00:18 -0400

    Update startTranscription.ts

--- pages/api/teams/startTranscription.ts
+++ pages/api/teams/startTranscription.ts
@@ -5,6 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).json({ error: 'Method not allowed' })
   }
 
+  
   try {
     const { meetingId, callId } = req.body
 
