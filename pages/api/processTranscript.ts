8fb16e953b959104a2565197c0b6f03bc363bc9d
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-06-23 19:45:36 -0400

    Update processTranscript.ts

--- pages/api/processTranscript.ts
+++ pages/api/processTranscript.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { meeting_id } = req.body;
 
