e8b6153aabd1d6c2c35a6e533230f79cc5b8e606
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-07-02 14:05:50 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-
+import Sidebar from '../components/Sidebar'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
+      <div style={{ marginLeft: '200px', padding: '2rem', width: '100%' }}>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
